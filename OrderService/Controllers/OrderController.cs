30c3476d583206c4a8224637724704a226318cd0
Pushp <Pushp@RADHEKRISHNA>
2025-08-20 11:44:05 +0530

    Add project files.

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(new[]
+            {
+                new { Id = 101, ProductId = 1, Quantity = 2 },
+                new { Id = 102, ProductId = 2, Quantity = 1 }
+            });
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(new[]
+            {
+                new { Id = 1, Name = "Laptop", Price = 800 },
+                new { Id = 2, Name = "Phone", Price = 500 }
+            });
+        }
+    }
+}
